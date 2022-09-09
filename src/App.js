c241dae74b364a313d9cb2564a0348639a7aaf59
Muhammad Waqas <ansariwaqas310@gmail.com>
2022-09-09 16:13:39 +0500

    scrolling text animation with gsap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import { gsap } from 'gsap'
+import './App.css'
+import { useRef, useEffect } from 'react'
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const element = ref.current
+    gsap.fromTo(
+      element.querySelector('.first-paragraph'),
+      {
+        opacity: 0.3,
+        y: 60,
+        x: window.innerWidth - 500,
+        transition: 0.3,
+        // transform: 'rotate(185deg)' ,
+        rotate: '145deg',
+        color: '#4a668b',
+      },
+      {
+        opacity: 1,
+        y: 0,
+        x: 0,
+        transition: 0.5,
+        rotate: '0deg',
+        color: '#794a8b',
+
+        // transform: 'rotate(105deg) !important'  ,
+
+        scrollTrigger: {
+          trigger: element.querySelector('.first'),
+          start: 'top top',
+          end: 'bottom center',
+          scrub: true,
+          pin: true,
+        },
+      },
+    )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" ref={ref}>
+      <header className="App-header first">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Hello</h1>
+        <p className="first-paragraph">I'm Waqas</p>
       </header>
+      <header className="App-header first2"></header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
